ec5c67b84f8ebb6f6e84cc905689be104da5077e
Gessielle <gessielle-barossi@hotmail.com>
2020-09-10 01:06:48 -0300

    olá usuário

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olá_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Gessielle!");
+        }
+    }
+}
